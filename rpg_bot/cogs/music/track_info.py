c2e3ece6bd2efc6f5abcc5b973a0493394e15678
Moésio Filho <moesio.f@gmail.com>
2022-06-17 16:59:14 -0300

    feat: add duration information to tracks

--- rpg_bot/cogs/music/track_info.py
+++ rpg_bot/cogs/music/track_info.py
@@ -3,6 +3,8 @@ import typing
 
 class TrackInfo:
   def __init__(self, url: str,
-               title: typing.Optional[str] = None):
+               title: typing.Optional[str] = None,
+               duration: typing.Optional[str] = None):
     self.url = url
     self.title = title
+    self.duration = duration
--- rpg_bot/utils.py
+++ rpg_bot/utils.py
@@ -4,48 +4,51 @@ import typing
 from rpg_bot.cogs.music.ost_key import OSTKey
 
 
-owner_ids = [300222466788425728, 419533052222373908,
-             335193595911077899, 952627911586877460]
-
-
 class ClientInput(typing.NamedTuple):
-  token: str
-  urls: typing.List[str]
+    token: str
+    urls: typing.List[str]
 
 
 def parse_arguments() -> ClientInput:
-  parser = argparse.ArgumentParser(description='A simple discord bot.')
-  parser.add_argument('-f', type=str, required=True,
-                      dest='url_filename')
-  parser.add_argument('-t', type=str, required=True,
-                      dest='bot_token')
-  args = parser.parse_args()
-  token: str = args.bot_token
-  url_fname = args.url_filename
+    parser = argparse.ArgumentParser(description='A simple discord bot.')
+    parser.add_argument('-f', type=str, required=True,
+                        dest='url_filename')
+    parser.add_argument('-t', type=str, required=True,
+                        dest='bot_token')
+    args = parser.parse_args()
+    token: str = args.bot_token
+    url_fname = args.url_filename
 
-  if token.endswith(".txt"):
-    with open(token) as file:
-      token = file.readline()
+    if token.endswith(".txt"):
+        with open(token) as file:
+            token = file.readline()
 
-  return ClientInput(token=token,
-                     urls=read_urls(url_fname))
+    return ClientInput(token=token,
+                       urls=read_urls(url_fname))
 
 
 def read_urls(fname: str) -> typing.Dict[OSTKey, typing.List[str]]:
-  dictionary = {k: [] for k in OSTKey}
-  with open(fname, 'r') as file:
-    for line in file:
-      splitted = line.split(":", 1)
-      key = OSTKey.from_str(splitted[0][0])
-      url = splitted[1]
-      dictionary[key].append(url)
+    dictionary = {k: [] for k in OSTKey}
+    with open(fname, 'r') as file:
+        for line in file:
+            splitted = line.split(":", 1)
+            key = OSTKey.from_str(splitted[0][0])
+            url = splitted[1]
+            dictionary[key].append(url)
 
-  return dictionary
+    return dictionary
 
 
-def check_is_owner(ctx):
-  return ctx.message.author.id in owner_ids
+def format_duration(total) -> str:
+    if total is None:
+        return ""
+
+    total = int(total)
+    minutes = total//60
+    seconds = total - 60*minutes
+
+    return f"{minutes:0>2}:{seconds:0>2}"
 
 
 def is_spotify_track(url):
-  return "open.spotify.com" in url and "track" in url
+    return "open.spotify.com" in url and "track" in url
